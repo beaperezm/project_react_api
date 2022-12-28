7aafa80d21f16aa2fc46fbb542e29f659b727eed
Jonathan Torres Maldonado <jonathan@MacBook-Air-de-Jonathan.local>
2022-12-28 11:31:45 +0100

    changed model

--- model/Series.js
+++ model/Series.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const serieSchema = new mongoose.Schema({
+    id: Number,
     title: { type: String, unique: true },
-    director: { type: String},
+    director: { type: [String]},
     year: { type: Number, min: 1895, max: 2023 },
-    genre: { type: [String], enum: ['Action', 'Adventure', 'Animated', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Historical', 'Horror', 'Noir', 'Science-fiction', 'Thriller', 'War', 'Western'] },
+    genre: { type: [String], enum: ['Action', 'Adventure', 'Animated', 'Biographic', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Historical', 'Horror', 'Mistery', 'Noir', 'Science-fiction', 'Soap Opera', 'Thriller', 'War', 'Western'] },
     picture: String,
     seasons: {type: Number},
-    platform: {type: String, enum:['HBO', 'Netflix','Disney', 'Amazon', 'A3Player', 'FOX']},
+    platform: {type: String, enum:['HBO', 'Netflix','Disney Plus+', 'Amazon', 'A3Player', 'FOX']},
     synopsis: {type: String},
     favoriteCount : Number
 },
